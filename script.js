f4035cac83521eef1d66218c4c5550f5c5fb3954
hanjala2p <mdhanjala016pc@gmail.com>
2025-08-25 21:29:44 +0600

    home page and add money page complete

---
+++ script.js
@@ -0,0 +1,26 @@
+// login btn
+document.getElementById('loginBtn').addEventListener('click',
+    function(e){
+        e.preventDefault()
+        const mobileNumber = 01604149169
+        const pinNumber = 1234
+        const mobileNumberValue =document.getElementById('mobileNumber').value
+        const mobileNumberValueConverted =parseInt(mobileNumberValue)
+
+        const pinNumberValue =document.getElementById('pinNumber').value
+        const pinNumberValueConverted =parseInt(pinNumberValue)
+
+        console.log(mobileNumberValueConverted,pinNumberValueConverted)
+
+        if(mobileNumberValueConverted===mobileNumber && pinNumberValueConverted===pinNumber){
+            window.location.href="/home.html"
+        }
+        else{
+            alert("Invalid Credentials")
+        }
+    }
+
+    
+)
+
+
