03e92b2b5be874d4731ca671ab10ba6edeb4b0c6
hanjala2p <mdhanjala016pc@gmail.com>
2025-09-07 22:26:26 +0600

    using reusable code

--- home.js
+++ home.js
@@ -21,14 +21,41 @@
 const validPin = 1234
 const validBankAccountNumber = 01604149169
 
+
+
+// re-usable function for input field
+
+function getInputValueNumber (id){
+   const inputField = document.getElementById(id)
+   const inputFieldValue = inputField.value
+   const inputFieldValueNumber = parseInt(inputFieldValue)
+  
+   return inputFieldValueNumber
+}
+// re-usable function for innertext
+
+function getInnerTextValueNumber (id){
+   const element = document.getElementById('balance')
+   const elementtValue = element.innerText
+   const elementValueNumber = parseInt(elementtValue)
+   return elementValueNumber
+}
+
+// re-usable function to set innerText
+
+function setInnerText (value){
+    const availableBalanceElement = document.getElementById('balance')
+    availableBalanceElement.innerText = value
+}
+
 document.getElementById('addMoney').addEventListener('click', function(e){
     e.preventDefault()
 
-    const bank = document.getElementById('bank').value 
+    const bank = getInputValueNumber('bank')
     const bankAccount =document.getElementById('bankAccount').value
     
-    const amount =parseInt(document.getElementById('addAmount').value)
-    const pin= parseInt(document.getElementById('password').value)
+    const amount =getInputValueNumber('addAmount')
+    const pin= getInputValueNumber('password')
     
     const availableBalance = parseInt(document.getElementById('balance').innerText)
     console.log(amount, availableBalance)
@@ -47,15 +74,16 @@ document.getElementById('addMoney').addEventListener('click', function(e){
         return;
     }
     const totalNewAvailableBalance = amount + availableBalance
-    document.getElementById('balance').innerText = totalNewAvailableBalance
+    // document.getElementById('balance').innerText = totalNewAvailableBalance
+    setInnerText(totalNewAvailableBalance)
    })
 // cash out  money feature
 document.getElementById("withdraw-money-btn").addEventListener('click',function(e){
     e.preventDefault()
-    const amount = parseInt(document.getElementById("WithdrawAmount").value)
-    const availableBalance =parseInt(document.getElementById("balance").innerText)
-    const WithdrawPassword =parseInt(document.getElementById("WithdrawPassword").value)
-    const bankAccountNumber =parseInt(document.getElementById("bankAccountNumber").value)
+    const amount = getInputValueNumber("WithdrawAmount")
+    const availableBalance =getInnerTextValueNumber("balance")
+    const WithdrawPassword = getInputValueNumber("WithdrawPassword")
+    const bankAccountNumber =getInputValueNumber("bankAccountNumber")
 
     const totalNewAvailableBalance = availableBalance - amount
     console.log(totalNewAvailableBalance)
@@ -72,7 +100,8 @@ document.getElementById("withdraw-money-btn").addEventListener('click',function(
          alert("please provide valid account number")
         return;
     }
-    document.getElementById("balance").innerText = totalNewAvailableBalance
+    // document.getElementById("balance").innerText = totalNewAvailableBalance
+    setInnerText(totalNewAvailableBalance)
 })
 
 //    toggling features
